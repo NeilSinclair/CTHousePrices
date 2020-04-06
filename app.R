73ee039b462e46f3fd97a92a53f1499d9bed19a4
NeilSinclair <neil.ian.sinclair@gmail.com>
2020-04-06 18:34:44 +0200

    Updated with area selection options

--- app.R
+++ app.R
@@ -3,6 +3,7 @@
 
 # Import the necessary libraries
 library(shiny)
+library(shinydashboard)
 library(ggplot2)
 library(tidyverse)
 library(sf)
@@ -10,53 +11,70 @@ library(leaflet)
 library(sp)
 library("RColorBrewer")
 
-?leafletProxy
-# The data has been processed by another script; load it in here
-load(file = "mapAppData.Rdata")
-combinded_data <- st_as_sf(combined_data)
 
+# The data has been processed by another script; load it in here
+#setwd("C:/Users/User/Documents/Python Scripts/Maps - Final")
+
+load(file = "./data/mapAppData.Rdata")
+combinded_data <- st_as_sf(combined_data)
+#view(combined_data[1:10,])
 
 # Create the Shiny App UI
-ui <- fluidPage(
+ui <- dashboardPage(
 
     # Application title
-    titlePanel("Cape Town suburb price data"),
-
-    sidebarLayout(
-        sidebarPanel(
+    dashboardHeader(title="Cape Town Suburb Price Data", titleWidth = 350),
+    
+    dashboardSidebar(disable = TRUE),
+    dashboardBody(
+      fluidRow(
+        column(5,
             # Explaination of what to do 
-            helpText("To view data only for specific suburbs, select the 'Selected suburbs'
-            radio button and then type the suburb name(s) in the Suburb box. Press 'backspace' to remove a 
-            suburb. To view price changes over time, use the slider below to select different time periods."),
+            helpText(HTML("<h4>Area selection overview</h4>
+            <ul><li>View average prices/price changes for all suburbs, suburbs by area or a selection 
+            of suburbs by ticking the appropriate radio button (Area display options) and searching in 
+            the appropriate search box (Area or Suburb)</li>
+            <li>To remove an item in the search bar, click on it and press backspace or delete </li>
+            <li>Use the slider below to select different time periods for the data in the map</li></ul>")),
             # Radio buttons for selection All suburbs or just specific suburbs to display on the map
-            radioButtons("radio", label = h4("Suburb display options"),
-                         choices = list("All suburbs" = 1, "Selected suburb(s)" = 2)),
+            radioButtons("radio", label = h4("Area display options"),
+                         choices = list("All suburbs" = 1, "Selected area(s)" = 2, 
+                                        "Selected suburb(s)" = 3), selected = 2, inline = TRUE),
+            # Drop down area-selection menu
+            selectInput("area", label = "Area", choices = combined_data$SUB_CNCL_NAME, 
+                        selected = c("Southern", "Atlantic, CBD and Surrounds"),
+                        multiple = TRUE, width = 600),
             # Drop down suburb-selection menu
-            selectInput("suburb", label = "Suburb", choices = combined_data$Suburb, selected = "",
-                        multiple = TRUE),
+            selectInput("suburb", label = "Suburb", choices = combined_data$Suburb, 
+                        selected = c("Pinelands","Woodstock","Mowbray","Salt River", "Vredehoek",
+                                     "Oranjezicht", "Thornton", "Athlone", "Observatory", "Gardens"),
+                        multiple = TRUE, width = 600),
             radioButtons("priceRadio", label = h4("Map data display options"),
-                         choices = list("Annualised % price change" = 1, "Average price over time period" = 2)),
+                         choices = list("Annualised % price change" = 1, 
+                                        "Average price over time period" = 2), inline = TRUE),
             # Slider to choose which years to see price change info for
-            sliderInput("range", label = h3("Time period"), min = 2011, 
-                           max = 2019, value = c(2017,2019), sep = "")
+            sliderInput("range", label = h4("Time period"), min = 2011, 
+                           max = 2019, value = c(2017,2019), sep = "", width = 600)
             ),
       
-        mainPanel(
-          helpText("This map visualises the compounded annual changes in average house prices over time or the
-                    average house price in the City of Cape Town, depending on
-                    which option is selected. Click on suburbs on the map to see the latest price info, the
-                   change in price over the time period selected and the suburb name."), 
+        column(7,
+          helpText("This map visualises the % change in house prices or the average prices over time in the 
+          suburbs in and around Cape Town, depending on which option is selected. 
+                    Click inside the suburbs on the map to see the suburb name along with the
+                    average price and the change in price over the time period selected."), 
           leafletOutput("suburbsMap")
            
         )
     )
+    )
 )
 
 server <- function(input, output) {
-    
+    # Render the map and then render new info on top of this (later in server) to reduce loading times
     output$suburbsMap <- renderLeaflet({
-        leaflet() %>% addProviderTiles("Esri.WorldStreetMap") %>%
-        setView(lng = 18.5, lat = -34, zoom = 10)
+        leaflet(options = leafletOptions(attributionControl=FALSE)) %>% 
+        addProviderTiles("Esri.WorldStreetMap") %>%
+        setView(lng = 18.5, lat = -33.95, zoom = 11)
 
     })
       
@@ -66,24 +84,27 @@ server <- function(input, output) {
       x1 <- input$range[1]
       x2 <- input$range[2]
       # Select the discrete (categorical) and continuous returns
-      return_range_D <- paste0("DX",x1,".",x2)
+
       return_range_C <- paste0("CX",x1,".",x2)
       
       # Select the data just years that are being compared by choosing the pre-calculated
       # column that stores the % return values
       
-      filtered_df <- combined_data %>% select(!!as.symbol(return_range_D), !!as.symbol(return_range_C),
-                                                Suburb, X2019, !!as.symbol(paste0("AvgX",x1,".",x2)))
-      names(filtered_df)[grep("^DX20", names(filtered_df))] <- "DReturn"
+      filtered_df <- combined_data %>% select(!!as.symbol(return_range_C),
+                                                Suburb, !!as.symbol(paste0("AvgX",x1,".",x2)),
+                                              SUB_CNCL_NAME)
+
       names(filtered_df)[grep("^CX20", names(filtered_df))] <- "CReturn"
       
       # Remove impossible (i.e. incorrect) values (< -1) and multiply by 100 so that it shows up correctly as a %
       filtered_df<- filtered_df %>% dplyr::filter(CReturn > -1)
       filtered_df$CReturn <- filtered_df$CReturn*100
-      filtered_df$X2019 <- substr(filtered_df$X2019/10^6, start = 1, stop = 4)
+   
       
       # If the "show all suburbs" button is not selected, then only show the selected suburbs
       if(input$radio == 2){
+        filtered_df <- filtered_df %>% filter(SUB_CNCL_NAME %in% input$area) 
+      } else if(input$radio == 3){
         filtered_df <- filtered_df %>% filter(Suburb %in% input$suburb) 
       }
       
@@ -95,29 +116,33 @@ server <- function(input, output) {
       observe({ 
         x1 <- input$range[1]
         x2 <- input$range[2]
+        legend_title <- paste0("Annualised % price <br> change: ",x1, " to ", x2)
         # If the user wants to see the change in price over time, show the following map
         if(input$priceRadio == 1){
-
-          pal <- colorFactor("RdYlGn", 
-                             levels = as.factor(c("< -25%", "-25% to -10%", "-10% to 0%",
-                                                  "0% to 10%", "10% to 25%", "> 25%")), ordered = TRUE)
+          percent.bins <- c(-100,-25,-10,0,10,25,300)
+          percent.labels <- c("< -25%", "-25% to -10%", "-10% to 0%",
+                               "0% to 10%", "10% to 25%", "> 25%")
+          
+          pal <- colorBin("RdYlGn", domain = filtered_data()$CReturn, bins = percent.bins)
           leafletProxy("suburbsMap", data = filtered_data()) %>% clearShapes() %>% removeControl("legend") %>%
-              addPolygons(data = filtered_data(), color = "black", weight = 1, fillColor = ~pal(DReturn), fillOpacity = 1,
+              addPolygons(data = filtered_data(), color = "black", weight = 1, fillColor = ~pal(CReturn), fillOpacity = 1,
                           popup = paste0("<b>Suburb: </b>", filtered_data()$Suburb, "<br>",
                           "<b>Annual price change ", x1, " to ", x2, ":</b> ", as.integer(filtered_data()$CReturn), 
-                          "%<br>", "<b>Average 2019 price: R</b>", filtered_data()$X2019, "mn")) %>%
-              leaflet::addLegend("topleft", colors = brewer.pal(n = 6, name = "RdYlGn"), 
-                        values = filtered_data()$DReturn,
-                      title = "Annual % \nprice change", opacity = 0.7,
-                        labFormat = labelFormat(suffix = ""),  
-                      labels = c("< -25%", "-25% to -10%", "-10% to 0%",
-                       "0% to 10%", "10% to 25%", "> 25%"), layerId = "legend")
+                          "%<br>", "<b>Average price from ",x1," to ", x2, ": R</b>",
+                          as.integer(filtered_data()$AvgPrice/10^4)/10^2, " mn")) %>%
+              leaflet::addLegend("bottomleft", colors = brewer.pal(n = length(percent.labels), name = "RdYlGn"), 
+                        values = filtered_data()$CReturn,
+                        title = HTML(legend_title), opacity = 0.7,
+                        labels = percent.labels, layerId = "legend")
         } else {
 ### --- if the user wants to see the average house price ver the period, show this map --- ###
           price.bins <- c(0,250*10^3, 500*10^3, 10^6, 2*10^6, 4*10^6, 8*10^6, 10^8)
           price.labels <- c("< R250k", "R250k - R500k", "R500k - R1m",
                            "R1m - R2m", "R2m - R4m", "R4m - R8m", "> R8m")
-
+          # Create a dynamic legend title
+          legend_title <- paste0("Average price between: <br> ",x1, " and ", x2)
+          
+          # Plot the map
           pal <- colorBin("RdYlGn", domain = filtered_data()$AvgPrice, bins = price.bins)
           leafletProxy("suburbsMap") %>% clearShapes() %>% removeControl("legend") %>%
             addPolygons(data = filtered_data(), color = "black", weight = 1, fillColor = ~pal(AvgPrice), fillOpacity = 1,
@@ -125,9 +150,9 @@ server <- function(input, output) {
                         "<b>Annual price change ", x1, " to ", x2, ":</b> ", as.integer(filtered_data()$CReturn),
                         "%<br>", "<b>Average price from ",x1," to ", x2, ": R</b>",
                         as.integer(filtered_data()$AvgPrice/10^4)/10^2, " mn")) %>%
-            leaflet::addLegend("topleft", colors = brewer.pal(n = length(price.labels), name = "RdYlGn"),
-                      values = filtered_data()$DReturn,
-                      title = "Average price over period", opacity = 0.7,
+            leaflet::addLegend("bottomleft", colors = brewer.pal(n = length(price.labels), name = "RdYlGn"),
+                      values = filtered_data()$AvgPrice,
+                      title = HTML(legend_title), opacity = 0.7,
                       labels = price.labels, layerId = "legend")
         }
         
--- map_processing.R
+++ map_processing.R
@@ -5,12 +5,13 @@ library(tidyverse)
 library(sf)
 library(sp)
 library("RColorBrewer")
-
+library(tools)
+#setwd("C:/Users/User/Documents/Python Scripts/Maps - Final")
 
 # Read in the house price data as scraped from the internet
-data1 <- read.csv("property_prices_ct.csv")
-data2 <- read.csv("property_prices_wp.csv")
-data3 <- read.csv("property_prices_wwp.csv")
+data1 <- read.csv("./data/property_prices_ct.csv")
+data2 <- read.csv("./data/property_prices_wp.csv")
+data3 <- read.csv("./data/property_prices_wwp.csv")
 
 data <- full_join(data3, data2)
 data <- full_join(data, data1)
@@ -22,6 +23,7 @@ data <- na.omit(data)
 
 data <- data %>% rename(Suburb = X)
 data$Suburb <- toupper(as.character(data$Suburb))
+
 # Remove the "Oudekraal" suburb as the data scraped is for a different region
 data <- data[-42,]
 
@@ -42,15 +44,8 @@ for (i in 2:9){
     # Calculate the annual mean compounding growth rate
     new_col <- exp(log(data[,j]/data[,i]))^(1/(j-i))-1
     data_temp <- cbind(data_temp, new_col)
-    # Create a column that stores the values as discrete categories as this makes the map
-    # and legend look better; keep the continuous values for pop-up displays
-    data_temp <- data_temp %>% mutate(new_cat = ifelse(new_col < -1, "NA", 
-                                                       ifelse(new_col < (-0.25), "< -25%", ifelse(new_col < (-0.1), "-25% to -10%",
-                                                                                                  ifelse(new_col < 0, "-10% to 0%", ifelse(new_col < 0.1, "0% to 10%",
-                                                                                                                                           ifelse(new_col < 0.25, "10% to 25%", "> 25%")))))))
-    # Replace the name of the temp columns with year-specific names; C -> continuous; D -> discrete
+
     names(data_temp)[grep("new_col", names(data_temp))] <- paste0("CX20",9+i,".20",9+j)
-    names(data_temp)[grep("new_cat", names(data_temp))] <- paste0("DX20",9+i,".20",9+j)
     
     # Calculate an average price column for the average price between years i and j
     avg_price_col <- (data[,j]+data[,i])/2
@@ -60,7 +55,7 @@ for (i in 2:9){
 }
 
 # Small suburb fix-ups for a few non-matching suburbs
-ct_suburbs[74, "Suburb"] = "CAMPS BAY"
+
 ct_suburbs[704, "Suburb"] = "CAMPS BAY"
 ct_suburbs[423, "Suburb"] = "SIMONS TOWN"
 ct_suburbs[37, "Suburb"] = "BO KAAP"
@@ -78,4 +73,35 @@ data_temp <- unique(data_temp)
 # between the two data sources
 combined_data <- st_as_sf(na.omit(inner_join( ct_suburbs, data_temp, by="Suburb")))
 
-save(combined_data, file="mapAppData.Rdata")
+# Processing of maps to create larger areas to allow for faster filtering on the map
+
+subcouncils <- st_read("./data/Sub_Councils.shp") %>% st_transform(crs = 4326)
+suburbs <- st_read("./data/Official_Suburbs.shp") %>% st_transform(crs = 4326)
+subcouncil_names <- read.csv("./data/subcouncil_names.csv")
+# Joins the two SF files, adding a column indicating which subcouncil each suburb falls into
+joined_map <- st_join(suburbs, subcouncils, left = FALSE, largest = TRUE)
+
+# Update some of the names in this joined data set so they match the other data sets
+joined_map$OFC_SBRB_N <- as.character(joined_map$OFC_SBRB_N)
+
+joined_map$OFC_SBRB_N[joined_map$OFC_SBRB_N == "SIMON'S TOWN"] <- "SIMONS TOWN"
+joined_map$OFC_SBRB_N[joined_map$OFC_SBRB_N == "CAMPS BAY / BAKOVEN"] <- "CAMPS BAY"
+joined_map$OFC_SBRB_N[joined_map$OFC_SBRB_N == "BO-KAAP"] <- "BO KAAP"
+joined_map$OFC_SBRB_N[joined_map$OFC_SBRB_N == "DISTRICT SIX"] <- "ZONNEBLOEM"
+
+# Exctract the data I need from the data.frame indicating which suburbs are in which councils
+subcouncils <- joined_map %>% select(OFC_SBRB_N, SUB_CNCL_1, SUB_CNCL_N)
+subcouncils <- data.frame(subcouncils)
+# Convert to remove unneccessary geometry column
+class(subcouncils) <- 'data.frame' 
+subcouncils <- subcouncils %>% select(-geometry) %>% rename(Suburb = OFC_SBRB_N)
+subcouncils$Suburb <- as.character(subcouncils$Suburb)
+
+# Add the sub-council names defined by me into the subcouncils table
+subcouncils <- left_join(subcouncils, subcouncil_names, by="SUB_CNCL_1")
+
+# Join data frames and save to file
+combined_data <- st_as_sf(na.omit(left_join(combined_data, subcouncils, by="Suburb")))
+combined_data$Suburb <- tools::toTitleCase(tolower(combined_data$Suburb))
+
+save(combined_data, file="./data/mapAppData.Rdata")
